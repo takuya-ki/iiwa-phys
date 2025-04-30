b3dbf3f488e5e2ed492746eadb6d66a1d1c9faec
takuya-ki <taku8926@gmail.com>
2025-04-30 20:16:32 +0900

    update the genesys_hello.py

--- scripts/genesys_hello.py
+++ scripts/genesys_hello.py
@@ -2,7 +2,17 @@ import genesis as gs
 
 gs.init(backend=gs.cpu)
 
-scene = gs.Scene()
+scene = gs.Scene(
+    show_viewer = True,
+    viewer_options = gs.options.ViewerOptions(
+        camera_pos = (3.5, -1.0, 2.5),
+        camera_lookat = (0.0, 0.0, 0.5),
+        camera_fov = 40,
+    ),
+    rigid_options = gs.options.RigidOptions(
+        dt = 0.01,
+    ),
+)
 
 plane = scene.add_entity(
     gs.morphs.Plane(),
@@ -15,6 +25,11 @@ franka = scene.add_entity(
     gs.morphs.MJCF(file="/dataset/xmls/iiwa/iiwa.xml"),
 )
 
-scene.build()
+B = 20
+scene.build(
+    n_envs=B, 
+    env_spacing=(1.0, 1.0)
+)
+
 for i in range(1000):
     scene.step()
